89fd9a2e09a5bedbfbb79a5fdde152437ac96c51
kjs <46150254+JsKim4@users.noreply.github.com>
2020-04-22 23:38:07 +0900

    Initial commit

---
+++ src/main/java/kr/kjs/dto/LottoDTO.java
@@ -0,0 +1,21 @@
+package kr.kjs.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter @ToString
+public class LottoDTO {
+	private String drwNo;
+	private String drwNo1;
+	private String drwNo2;
+	private String drwNo3;
+	private String drwNo4;
+	private String drwNo5;
+	private String drwNo6;
+	private String bnusNo;
+	private String drwNoDate;
+	private String firstWinamnt;
+	private String firstPrzwnerCo;
+	private String firstAccumant;
+}
